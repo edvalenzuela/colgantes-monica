968110f3ef7acde8b6d207181f6ee963c6bd84f9
edvalenzuela <edvalenzuelap@gmail.com>
2025-02-09 04:24:01 -0300

    feat: add each for show section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { About, Footer, Gallery} from "@/components";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold">
-      Colgantes Mónica
-    </h1>
+    <>
+      <main>
+        <About />
+        <Gallery />
+        <Footer />
+      </main>
+    </>
   );
 }
