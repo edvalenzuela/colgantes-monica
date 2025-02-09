ea8556a416671047310a84f49f793c6e4e6352fb
edvalenzuela <edvalenzuelap@gmail.com>
2025-02-09 07:15:35 -0300

    fix: add the same email for resend validate

--- src/resend/index.ts
+++ src/resend/index.ts
@@ -7,13 +7,16 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ email, subject, message }:InitialValues) => {
   try {
     await resend.emails.send({
-      from: email,
+      from: 'contacto@colgantesmonica.cl',
       to: 'contacto@colgantesmonica.cl',
       subject: subject,
-      html: `<p>${message}</p>`,
+      html: `<p><strong>Correo del remitente:</strong> ${email}</p>
+         <p><strong>Mensaje:</strong> ${message}</p>`
     });
   } catch (error) {
     console.log(error)
     throw new Error('Error al enviar el correo');
   }
 }
+
+
