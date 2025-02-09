48463ae3cf2dbdebbdb9d6f75cd639b581781a38
edvalenzuela <edvalenzuelap@gmail.com>
2025-02-09 07:45:48 -0300

    style: add missing padding

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -37,7 +37,7 @@ export default function ContactForm() {
 
   return (
     <section className="min-h-screen flex items-center justify-center text-gray-600 body-font relative bg-white" id="contacto">
-      <div className="container px-5 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="container p-5 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="w-full lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
