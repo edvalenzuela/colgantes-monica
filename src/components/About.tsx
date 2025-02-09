02aff55b23294a019c39c46c33f1d3b2f254aa78
edvalenzuela <edvalenzuelap@gmail.com>
2025-02-09 14:42:08 -0300

    style: remove border-radius

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import { FcBusinesswoman, FcLike } from "react-icons/fc";
 export default function About() {
   return (
     <section id="quien-soy" className="min-h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat bg-fixed bg-header">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center bg-black/50 shadow-lg rounded-2xl">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center bg-black/50 shadow-lg rounded-none md:rounded-2xl">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="text-4xl mb-5 font-bold text-white flex items-center gap-2">
             <FcBusinesswoman /> Quién soy?
