cc2220371e0971386fa6cdeb918372e073fe9dca
edvalenzuela <edvalenzuelap@gmail.com>
2025-02-14 02:27:04 -0300

    feat: add size in svg icons

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import { Story } from "@/components";
 import { FcBusinesswoman, FcLike } from "react-icons/fc";
 
 export default function About() {
   return (
     <section id="quien-soy">
-      <Story />
-      <div className="container mx-auto flex my-15 px-5 py-24 md:flex-row flex-col justify-center items-center shadow-xl rounded-none md:rounded-2xl">
+      <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col justify-center items-center shadow-xl rounded-none md:rounded-2xl">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="text-4xl mb-5 font-bold flex items-center gap-2">
-            <FcBusinesswoman /> Quién soy?
+            <FcBusinesswoman size={40} /> Quién soy?
           </h1>
           <p className="mb-8 drop-shadow-xl text-left leading-relaxed text-xl">
           Soy Mónica Peña, nací en 1964 y a lo largo de mi vida he trabajado en diversas áreas. Tras dejar el trabajo corporativo, decidí cambiar de rumbo y unirme al grupo de Artesanos Unidos de la Municipalidad de San Bernardo, donde descubrí mi pasión por crear colgantes para plantas. Me encanta dar vida a los espacios con mis creaciones y siempre busco inspiración en lo cotidiano. ¡Cada día es una oportunidad para seguir aprendiendo y creciendo!
