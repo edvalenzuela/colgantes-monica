8bc0f6922351e471c3464af1c914854a39ee0bf5
edvalenzuela <edvalenzuelap@gmail.com>
2025-02-14 02:27:48 -0300

    feat: add active menu

--- src/components/Navbarv2.tsx
+++ src/components/Navbarv2.tsx
@@ -1,22 +1,24 @@
 "use client"
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
-import Link from 'next/link'
 import { useState } from 'react'
+import Link from 'next/link'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import CustomSVGIcon from './SvgIcon'
 
 const navigation = [
-  { name: 'Inicio', href: '/', current: true },
-  { name: 'Quien soy', href: 'quien-soy', current: false },
-  { name: 'Galería', href: 'galería', current: false },
-  { name: 'Contacto', href: 'contacto', current: false },
-]
+  { name: 'Inicio', href: '/', id: 'inicio' },
+  { name: 'Quien soy', href: '#quien-soy', id: 'quien-soy' },
+  { name: 'Galería', href: '#galería', id: 'galería' },
+  { name: 'Contacto', href: '#contacto', id: 'contacto' },
+];
 
 export default function Navbarv2() {
   const [isOpen, setIsOpen] = useState(false)
+  const [activeMenu, setActiveMenu] = useState("inicio")
 
-  function isCurrentMenu(...classes:string[]):string {
-    return classes.filter(Boolean).join(' ')
+  function handleMenuClick(id: string) {
+    setActiveMenu(id);
+    setIsOpen(false);
   }
 
   return (
@@ -35,23 +37,21 @@ export default function Navbarv2() {
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
               <Link href='/' className='group' title="Colgantes Mónica">
-              <div className="h-15 w-15 sm:h-12 sm:w-12 hover:bg-white p-1 rounded-full transition">
-                <CustomSVGIcon />
-              </div>
+                <div className="h-15 w-15 sm:h-12 sm:w-12 hover:bg-white p-1 rounded-full transition">
+                  <CustomSVGIcon />
+                </div>
               </Link>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                {navigation.map(({name, href, current}) => (
+                {navigation.map(({name, href, id}) => (
                   <Link
                     key={name}
-                    href={href === "inicio" ? "/" : `/#${href}`}
-                    aria-current={current ? 'page' : undefined}
-                    className={isCurrentMenu(
-                      current ? 'bg-green-700 text-white' : 'text-white hover:bg-green-700 hover:text-white',
-                      'rounded-md p-3 text-base font-medium',
-                    )}
-                    onClick={() => setIsOpen(false)}
+                    href={href}
+                    onClick={() => handleMenuClick(id)}
+                    className={`rounded-md p-3 text-base font-medium ${
+                      activeMenu === id ? "bg-green-700 text-white" : "text-white hover:bg-green-700 hover:text-white"
+                    }`}
                   >
                     {name}
                   </Link>
@@ -62,19 +62,18 @@ export default function Navbarv2() {
         </div>
       </div>
 
-      <DisclosurePanel className={`absolute left-0 w-full sm:invisible bg-repeat bg-texture-green ${isOpen ? 'visible' : 'invisible'}`}>
+      {/* Menú en versión móvil */}
+      <DisclosurePanel className={`absolute left-0 w-full sm:invisible bg-repeat bg-texture-green ${isOpen ? "visible" : "invisible"}`}>
         <div className="space-y-1 px-2 pt-2 pb-3">
-          {navigation.map(({name, href, current}) => (
+          {navigation.map(({name, href, id}) => (
             <DisclosureButton
               key={name}
               as="a"
-              href={href === "inicio" ? "/" : `/#${href}`}
-              aria-current={current ? 'page' : undefined}
-              className={isCurrentMenu(
-                current ? 'bg-green-700 text-white' : 'text-white hover:bg-green-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
-              )}
-              onClick={() => setIsOpen(false)}
+              href={href}
+              onClick={() => handleMenuClick(id)}
+              className={`block rounded-md px-3 py-2 text-base font-medium ${
+                activeMenu === id ? "bg-green-700 text-white" : "text-white hover:bg-green-700 hover:text-white"
+              }`}
             >
               {name}
             </DisclosureButton>
